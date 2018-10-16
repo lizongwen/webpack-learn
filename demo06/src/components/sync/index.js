8025281172b950ae9b2f7f0ff0975495cf5edcb9
lizongwen <93700983@qq.com>
2018-10-16 15:22:50 +0800

    enter

--- demo06/src/components/sync/index.js
+++ demo06/src/components/sync/index.js
@@ -1,5 +1,5 @@
-import lodash from "lodash-es";
-// import {isArray} from "lodash-es";
+// import lodash from "lodash-es";
+import {isArray} from "lodash-es";
 import item from "./sync.css";
 import help from "../common/help.js";
 console.log("async引用",help.version);
@@ -12,8 +12,8 @@ const sync = function(){
 	})
 }
 const isArrayFun = function(args){
-    console.log(lodash.isArray(args));
-    // console.log(isArray(args));
+    // console.log(lodash.isArray(args));
+    console.log(isArray(args));
 }
 export {
     sync,
