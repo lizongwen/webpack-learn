29ecc8e7642c11ede79dec570f62b3ce8d2b205b
lizongwen <93700983@qq.com>
2018-10-17 16:15:53 +0800

    enter

--- demo07/project/src/app.controller.ts
+++ demo07/project/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 	constructor(private readonly appService: AppService) { }
 
 	@Get()
-	@Render("index.html")
+	@Render("index.entry.html")
 	root(): object {
 		return { message: this.appService.root() };
 	}
